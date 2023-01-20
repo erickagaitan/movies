096e9676c5294908ef5e56a7162046f39b37273f
erickagaitan <gaitanericka@gmail.com>
2023-01-20 15:15:11 -0300

    configuración en api

--- src/page/Details/Details.jsx
+++ src/page/Details/Details.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react";
 import {useParams} from "react-router-dom"
 import { getDetails } from "../../api/movie";
+
 import "./details.css"
 
 const Details=()=> {
