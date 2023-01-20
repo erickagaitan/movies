3960489083283c7fe0e45baee34b307e193aa195
erickagaitan <gaitanericka@gmail.com>
2023-01-20 15:37:18 -0300

    :wrench:  configuración footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -9,7 +9,11 @@ const Footer=()=> {
         <span className="texto-footer">By Ericka Gaitan ® 2023</span>
 
         <a href="https://github.com/erickagaitan/movies">
-          <img src={IconGithub} className="icon-github" />
+          <img 
+            src={IconGithub} 
+            className="icon-github" 
+            alt="icon-github"
+          />
         </a>
        
       </div>
